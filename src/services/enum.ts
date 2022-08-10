3e6672237e26f13a1e92268d205cba91798cdab1
johnsonfash <fashanutosin7@gmail.com>
2022-08-10 07:24:55 +0100

    changed api url

--- src/services/enum.ts
+++ src/services/enum.ts
@@ -1,5 +1,5 @@
 export enum ENUM {
   ACCESS_TOKEN = 'access_token',
   REFRESH_TOKEN = 'refresh_token',
-  GRAPH_URL = 'http://localhost:5000/graphql'
+  GRAPH_URL = 'https://crud-test-backend.herokuapp.com/graphql'
 }
